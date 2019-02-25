841a025a093aed7ed21e5773146832add22fdcb3
sujiko <qureshi.ak225@gmail.com>
2019-02-25 10:35:53 -0500

    basic template for the website
    most of the coding will be done in the body section of this template

--- tracker.php
+++ tracker.php
@@ -1,1 +1,11 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+       <link rel="stylesheet" href = "style.css">
+       <title> Timebank </title>
+       <meta charset = "utf-8">
+    </head>
+    <body>
+    </body>
+</html>
 
