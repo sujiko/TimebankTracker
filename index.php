538c103e4bc9591ac8e304f1d303ee006be9429b
Anna Malyevac <amalyeva@cs.umw.edu>
2019-03-17 23:00:38 -0400

    Simple hompage with login links

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
   <meta charset="UTF-8">
-  <title>index</title>
-  <h1>its the index</h1>
+  <div>
+  <center>
+  <title>Extension Tracker</title>
+  <h1>Welcome to Dr. Zeitz's Extension Tracker</h1>
+ <!-- <input type= "submit" value = "Admin Login">-->
+  <a href= "adminLogin.php"> Admin Login </a>
+  &nbsp;
+  &nbsp;
+ <!-- <input type= "submit" value = "Student Login"> -->
+  <a href= "studentLogin.php">Student Login </a>
+  </center>
+  </div>
 </html>
