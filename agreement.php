49bd40a806160016839faa51e0f9bf3bafd6e84e
Christian Watt <cwatt@cs.umw.edu>
2019-03-20 11:24:56 -0400

    changed student login to reflect db changes and added redirect to agreement if first login and redirect to agreement if second login

---
+++ agreement.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>agreement </title>
+    </head>
+    <body>
+        <h1> agreement </h1>
+        <p lang="en"> Hate to break it to you, but what people call "love" is just a chemical
+            reaction that compels animals to breed. It hits hard Morty then it slowly 
+            fades leaving you stranded in a failing marriage. I did it. Your parents 
+            are going to do it. Break the cycle Morty, rise above, focus on science.
+        </p>
+    </body>
+</html>
+
