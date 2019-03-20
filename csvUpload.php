966098c8883e961781e58d3703739af15d5b842e
Christian Watt <cwatt@cs.umw.edu>
2019-03-20 12:20:15 -0400

    fixed so script runs on submit only

--- csvUpload.php
+++ csvUpload.php
@@ -1,37 +1,42 @@
 <?php
 session_start();
 if(!isset($_SESSION["username"])){ //if login in session is not set
-    header("Location: adminLogin.php");
+  header("Location: adminLogin.php");
 }
 // check there are no errors
-if($_FILES['csv']['error'] == 0){
-	//assign the file name to a var
+// need post so script runs on submit only
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if($_FILES['csv']['error'] == UPLOAD_ERR_OK){
+    //assign the file name to a var
     $name = $_FILES['csv']['name'];
-	//assign the file extention to a var
+    //assign the file extention to a var
     $ext = strtolower(end(explode('.', $_FILES['csv']['name'])));
-	//get the file type in a var
+    //get the file type in a var
     $type = $_FILES['csv']['type'];
-	//get get serverside name of file in a var
+    //get get serverside name of file in a var
     $tmpName = $_FILES['csv']['tmp_name'];
-
     // check the file is a csv
     if($ext === 'csv'){
-	//check to make sure you can open the csv and assign it to a var
-        if(($handle = fopen($tmpName, 'r')) !== FALSE) {
-		//WE DID IT YA'LL
-           echo "THATS A CSV";
-	}
-	else{
-		//something wasn't right with the csv
-	   echo "could not open";
-	}
+      //check to make sure you can open the csv and assign it to a var
+      if(($handle = fopen($tmpName, 'r')) !== FALSE) {
+        //WE DID IT YA'LL
+        //echo "THATS A CSV";
+      }
+      else{
+        //something wasn't right with the csv
+        echo "could not open";
+      }
     }
     else{
-	//thats not a csv mate
-        echo "not a csv";
+      //thats not a csv mate
+      echo "not a csv";
     }
+  }
+  else{
+    //thats not a csv mate
+    echo "not a csv";
+  }
 }
-
 ?>
 <html>
   <meta charset="UTF-8">
