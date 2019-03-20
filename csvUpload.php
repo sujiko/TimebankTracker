b70611e6340f9a6913c244d83f97e0978d712ced
Christian Watt <cwatt@cs.umw.edu>
2019-03-20 17:20:32 -0400

    can use csv to add students small debugging issue with last names having too many spaces if there isnt a comma after it in the csv

--- csvUpload.php
+++ csvUpload.php
@@ -6,9 +6,16 @@ if(!isset($_SESSION["username"])){ //if login in session is not set
 // check there are no errors
 // need post so script runs on submit only
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  include '../../conf.php';
+  $dbhost = $host;
+  $dbuser = $user;
+  $dbpass = $password;
+  $db = $database;
   if($_FILES['csv']['error'] == UPLOAD_ERR_OK){
     //assign the file name to a var
     $name = $_FILES['csv']['name'];
+    $className = explode('.',$_FILES['csv']['name']);
+    $class = $className[0];
     //assign the file extention to a var
     $ext = strtolower(end(explode('.', $_FILES['csv']['name'])));
     //get the file type in a var
@@ -17,8 +24,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $tmpName = $_FILES['csv']['tmp_name'];
     // check the file is a csv
     if($ext === 'csv'){
+      $conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
       //check to make sure you can open the csv and assign it to a var
-      if(($handle = fopen($tmpName, 'r')) !== FALSE) {
+      if(($myfile = fopen($tmpName, 'r')) !== FALSE) {
+        while(!feof($myfile)){
+          $line=fgets($myfile);
+          $lineArray = explode(',',$line);
+          $fixLastName = explode('\\',$lineArray[2]);
+          $lineArray[2] = $fixLastName[0];
+          //echo $lineArray[2].strlen($lineArray[2]).'<br>';
+          $sql = "insert into students values('".$conn->real_escape_string($lineArray[0])."','".$conn->real_escape_string($lineArray[1])."','".$conn->real_escape_string($lineArray[2])."',ENCODE('".$conn->real_escape_string($lineArray[0])."','".$crypt_str."'),'".$class."',0,3)";
+          $result = $conn->query($sql);
+          if (!$result) {
+            die("Error executing query: ($conn->errno) $conn->error");
+          }
+        }
+        fclose($myfile);
         //WE DID IT YA'LL
         //echo "THATS A CSV";
       }
@@ -31,10 +52,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       //thats not a csv mate
       echo "not a csv";
     }
-  }
-  else{
-    //thats not a csv mate
-    echo "not a csv";
   }
 }
 ?>
