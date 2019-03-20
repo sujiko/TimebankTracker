0b1466cad28376a646dcaaa10a2e94a9b95fc701
Anna Malyevac <amalyeva@cs.umw.edu>
2019-03-20 11:23:30 -0400

    Added link to admin homepage from admin login. Added upload csv as well

--- adminHome.php
+++ adminHome.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
-	<html>
-		<head>
-			<link rel="stylesheet" href = "style.css">
-                        <title> Timebank Tracker</title>
-                        <meta charset = "utf-8">
-                </head>
-		<body>
-			<h1> Admin Homepage </h1>
-		</body>
-		<?php
-		?>		
-	</html>
+<html>
+<head>
+<link rel="stylesheet" href = "style.css">
+<title> Timebank Tracker</title>
+<meta charset = "utf-8">
+</head>
+<body>
+<h1> Admin Homepage </h1>
+<a href= "csvUpload.php">Upload a CSV </a>
+</body>
+<?php
+
+?>
+		
+</html>
 
