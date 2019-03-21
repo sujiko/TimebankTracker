aac0a1096c9964f1d7f0f80ba61f9d1f2f7d83bd
Anna Malyevac <amalyeva@cs.umw.edu>
2019-03-21 16:52:05 -0400

    Added some non-working (currently) features to the TOS

--- agreement.php
+++ agreement.php
@@ -1,15 +1,24 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>agreement </title>
+	<link rel="stylesheet" href = "style.css">
+        <title>Terms of Service Agreement </title>
     </head>
     <body>
-        <h1> agreement </h1>
+        <h1> Terms of Service </h1>
         <p lang="en"> Hate to break it to you, but what people call "love" is just a chemical
             reaction that compels animals to breed. It hits hard Morty then it slowly 
             fades leaving you stranded in a failing marriage. I did it. Your parents 
             are going to do it. Break the cycle Morty, rise above, focus on science.
         </p>
+	<form action = "#" onsubmit= "if(document.getElementByID('agree').chekced) { return true; } else alert('Please check that you have agreed to the Terms and Conditions'); return false; }">
+	<input type="checkbox" name="checkbox" value="check" id="agree" /> I have read and agree to the Terms and Conditions
+	<input type="submit" name="submit" value="submit" />
+	<div>
+	<center>
+	<label>New Password: <input type="text" name="password"></label>
+	</center>
+	</div>
     </body>
 </html>
 
