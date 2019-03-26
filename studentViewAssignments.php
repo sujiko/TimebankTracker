3a2800fb1f1e6f8d494316e6d4e065cfc195f997
Anum Qureshi <aquresh2@cs.umw.edu>
2019-03-26 13:11:18 -0400

    fixed a spelling error for studentViewAssignment to studentViewAssignments

---
+++ studentViewAssignments.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<?php
+session_start();
+if(!isset($_SESSION["username"])){ //if login in session is not set
+  header("Location: studentLogin.php");
+}
+?>
+<html>
+<div class="navbar">
+  <a href="studentView.php">Home</a>
+  <div class="dropdown">
+    <button class="dropbtn">Timebank Days</button>
+    <div class="dropdown-content">
+      <a href="useTimebank.php">Use a Timebank Day</a>
+    </div>
+  </div>
+  <div class="dropdown">
+    <button class="dropbtn">Assignment</button>
+    <div class="dropdown-content">
+      <a href="studentViewAssignments.php">View Assignments</a>
+    </div>
+  </div>
+  <div class="dropdown">
+    <button class="dropbtn">Account</button>
+    <div class="dropdown-content">
+      <a href="studentSettings.php">Settings</a>
+      <a href="logout.php">Logout</a>
+    </div>
+  </div>
+</div>
+<head>
+<link rel="stylesheet" href = "home.css">
+<title>Assignments</title>
+<meta charset = "utf-8">
+<h1> Assignments List </h1>
+</head>
+</html>
+
