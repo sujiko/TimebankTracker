ad210b04e43cff096428584d74eb43852c3e76b4
Anum Qureshi <aquresh2@cs.umw.edu>
2019-04-03 22:38:56 -0400

    fixed student homepage to also display when assignment is really due and class

--- studentView.php
+++ studentView.php
@@ -46,17 +46,19 @@ if(!isset($_SESSION["pid"])){ //if login in session is not set
     $result = $conn-> query($sql);
     $row = $result->fetch_assoc();
   echo "<p class = 'warning'> YOU HAVE ".$row['days']." TIMEBANK DAYS LEFT! </p>";
+        $sqlClass = "SELECT class FROM students WHERE pid = '".$_SESSION['pid']."'";
+        $resultClass = $conn->query($sqlClass);
+ 	while($classRow = $resultClass->fetch_assoc()){
+        echo "<p style= 'text-align:center;' > class: ".$classRow['class']."</p>";
     echo "<table>";
-    echo "<tr><th>Assignment Name </th><th>Due Date</th>";
-     $newSql = "SELECT distinct assignmentName, initDue, newDueDate FROM assignments WHERE pid ='".$_SESSION['pid']."' ";
+    echo "<tr><th>Assignment Name </th><th>Due Date</th><th>Your Due Date</th>";
+     $newSql = "SELECT assignmentName, initDue, newDueDate FROM assignments WHERE pid ='".$_SESSION['pid']."' AND class='".$classRow['class']."'";
      $newResult = $conn->query($newSql);
      while($curRow = $newResult->fetch_assoc()){
-       // echo "<input type='radio name= 'assignment'"
-       // if (isset($assignment) && $assignment== $curRow["assignmentName"]) echo "checked";
-       // echo "value = '".$curRow['assignmentName']."' > ".$curRow["assignmentName"]."";
-      echo "<tr><td>".$curRow['assignmentName']."</td><td>".$curRow['initDue']."</td>";
+      echo "<tr><td>".$curRow['assignmentName']."</td><td>".$curRow['initDue']."</td><td>".$curRow['newDueDate']."</td>";
         }
       echo "</table><br>";
+      }
 ?>
 
 </body>
